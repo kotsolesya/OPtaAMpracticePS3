273aeab80e5d23f07c276afd97e0af61790d47fa
zakeratt <zakeratt@gmail.com>
2016-11-23 10:26:06 +0200

    fix

---
+++ 16.12 funkcii 3 den.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+float y(float);
+int main()
+{
+    float i, x, f, a, b, h;
+    for (x = -7; x <=7; x += 0.75 )
+   { 
+        cout << " y = "<<y(x)<<endl;
+   }
+        system("pause");
+        return 0;
+   }
+float y(float x){
+return (abs(3*x-cos(x))/((x*x)-9));  
+}  
